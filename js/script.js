dd432354fe29dc4ae38ba00fe14818adef96f023
Connie <clai011@ucr.edu>
2019-08-09 00:15:43 -0700

    need to add picking feature

--- js/script.js
+++ js/script.js
@@ -1,35 +1,25 @@
 let ogBoard;
-const player = 'O';
+const you = 'O';
 const ai = 'X';
 const winCombos = [
-    // horizontal, vertical, diagonal
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
-
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-
     [0, 4, 8],
     [6, 4, 2]
-
 ]
-// select all elements with class = "cell"; array
+
 const cells = document.querySelectorAll('.cell');
-
-const resetButton = document.querySelector('.resetBtn');
-resetButton.addEventListener('click', startGame);
-
-
-// for Reset/Play Again button
+const resetButton = document.querySelector('.resetBtn').addEventListener('click', startGame);
 startGame();
 
-function startGame() {
-    // clear endgame section
-    document.querySelector('.endgame').style.display = "none";
-    //create array of 9 elements
+function startGame() { // called when resetBtn pressed
+    // clear endgame board
+    document.querySelector(".endgame").style.display = "none";
     ogBoard = Array.from(Array(9).keys());
-    //remove all X/Os from board
     for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = '';
         cells[i].style.removeProperty('background-color');
@@ -38,34 +28,24 @@ function startGame() {
 }
 
 function turnClick(square) {
-    // console.log(square.target.id);
-    // call turn func when human player clicks
-    if (typeof ogBoard[square.target.id] == 'number') {//if cell is empty
-        turn(square.target.id, player);
-        if (!checkTie()) turn(bestSpot(), ai);
+    if (typeof ogBoard[square.target.id] == 'number') {
+        turn(square.target.id, you)
+        if (!checkWin(ogBoard, you) && !checkTie()) turn(bestSpot(), ai);
     }
-
 }
 
 function turn(squareId, player) {
     ogBoard[squareId] = player;
-    // puts 'O' on a cell that player clicks on
     document.getElementById(squareId).innerText = player;
-    let gameWon = checkWin(ogBoard, player);
-    if (gameWon) gameOver(gameWon);
-
+    let gameWon = checkWin(ogBoard, player)
+    if (gameWon) gameOver(gameWon)
 }
-// determine winner
+
 function checkWin(board, player) {
-    // acc - returned value; if element = player, concat i(index) to acc
-    // [] is initialValue argument in .reduce()
-    // FIND EVERY INDEX THE PLAYER PLAYED IN
     let plays = board.reduce((acc, ele, i) =>
         (ele === player) ? acc.concat(i) : acc, []);
-
     let gameWon = null;
     for (let [index, win] of winCombos.entries()) {
-        // has player played on every cell that counts as a win?
         if (win.every(ele => plays.indexOf(ele) > -1)) {
             gameWon = { index: index, player: player };
             break;
@@ -75,35 +55,34 @@ function checkWin(board, player) {
 }
 
 function gameOver(gameWon) {
-    //change bg color based on winner
     for (let index of winCombos[gameWon.index]) {
-        document.getElementById(index).style.backgroundColor = gameWon.player == player ? "blue" : "red"
+        document.getElementById(index).style.backgroundColor =
+            gameWon.player == you ? "blue" : "red";
     }
-    // prevent player from clicking after winning
     for (let i = 0; i < cells.length; i++) {
-        cells.removeEventListener
+        cells[i].removeEventListener('click', turnClick, false);
     }
-    declareWinner(gameWon.player == player ? "YAY, You Win!" : "Aww, You Lose!")
+    declareWinner(gameWon.player == you ? "You win!" : "You lose.");
 }
 
-// AI & winner notification
 function declareWinner(who) {
-    document.querySelector('.endgame').style.display = 'block';
-    document.querySelector('.endgame .text').innerText = who;
+    document.querySelector(".endgame").style.display = "block";
+    document.querySelector(".endgame .text").innerText = who;
 }
+
 function emptySquares() {
     return ogBoard.filter(s => typeof s == 'number');
 }
+
 function bestSpot() {
-    // find first square that's not empty
-    return emptySquares()[0];
+    return minimax(ogBoard, ai).index;
 }
 
 function checkTie() {
     if (emptySquares().length == 0) {
         for (let i = 0; i < cells.length; i++) {
-            cells[i].style.backgroundColor = 'green';
-            cells[i].removeEventListener('click', turnClick, false)
+            cells[i].style.backgroundColor = "green";
+            cells[i].removeEventListener('click', turnClick, false);
         }
         declareWinner("Tie Game!")
         return true;
@@ -111,7 +90,53 @@ function checkTie() {
     return false;
 }
 
+function minimax(newBoard, player) {
+    let openSpots = emptySquares();
 
+    if (checkWin(newBoard, you)) {
+        return { score: -10 };
+    } else if (checkWin(newBoard, ai)) {
+        return { score: 10 };
+    } else if (openSpots.length === 0) {
+        return { score: 0 };
+    }
+    let moves = [];
+    for (let i = 0; i < openSpots.length; i++) {
+        let move = {};
+        move.index = newBoard[openSpots[i]];
+        newBoard[openSpots[i]] = player;
 
+        if (player == ai) {
+            let result = minimax(newBoard, you);
+            move.score = result.score;
+        } else {
+            let result = minimax(newBoard, ai);
+            move.score = result.score;
+        }
 
-// algorithm
+        newBoard[openSpots[i]] = move.index;
+
+        moves.push(move);
+    }
+
+    let bestMove;
+    if (player === ai) {
+        let bestScore = -10000;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        let bestScore = 10000;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+
+    return moves[bestMove];
+}
